e965c90e85127faea7ac54b903ab0c35ba24338b
drsaffas <djsaffas014@gmail.com>
2023-09-09 13:38:26 +0100

    An endpoint that returns a JSON response to the user.

---
+++ endpoint/endpoint.php
@@ -0,0 +1,37 @@
+<?php
+
+// Parse the GET parameters from the url
+
+$slackName = isset($_GET['slack_name']) ? $_GET['slack_name'] : '';
+$track = isset($_GET['track']) ? $_GET['track'] : '';
+
+// Get the current day of the week and UTC time
+
+$currentDay = date('l'); //Full day name
+$utcTime = gmdate('Y-m-d\TH:i:s\Z');
+
+// Construct a JSON response
+
+$response = array(
+    "slack_name" => $slackName,
+    "current_day" => $currentDay,
+    "utc_time" => $utcTime,
+    "track" => $track,
+    "github_file_url" => "https://github.com/drsaffas/backend-internship.git",
+    "github_repo_url" => "https://github.com/drsaffas/backend-internship.git",
+    "status_code" => 200
+);
+
+// Convert the response array to JSON
+
+$jsonResponse = json_encode($response);
+
+// Set the Content-Type header to indicate JSON response
+
+header('Content-Type: application/json');
+
+// Return the JSON response
+
+echo $jsonResponse;
+
+?>
